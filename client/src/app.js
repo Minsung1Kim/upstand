a7c1096ae90f61b9e8980d2707f8901942a3ed31
Minsung Kim <130531502+Minsung1Kim@users.noreply.github.com>
2025-07-29 12:07:11 -0700

    Update app.js

--- client/src/app.js
+++ client/src/app.js
@@ -54,7 +54,7 @@ function App() {
 // Layout wrapper with Navbar
 function WithNavbar() {
   return (
-    <>
+    <> 
       <Navbar />
       <main className="container mx-auto px-4 py-8">
         <Outlet />
